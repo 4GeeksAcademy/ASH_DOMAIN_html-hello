df010fd941823b624066affbe94c033be4db96e7
agusotoholt <168455366+agusotoholt@users.noreply.github.com>
2024-07-01 14:36:10 +0000

    primera subida

---
+++ app.js
@@ -0,0 +1,19 @@
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+
+for (let i=0; i < pronoun.length; i++) {
+    for (j=0; j < adj.length; j++) {
+        for (k=0; k < noun.length; k++) {
+            console.log(pronoun[i]+adj[j]+noun[k]+".com")
+        }
+    }
+}
+
+for (let i=0; i < pronoun.length; i++) {
+    for (j=0; j < adj.length; j++) {
+        for (k=0; k < noun.length; k++) {
+            console.log(pronoun[i]+adj[j]+"."+noun[k])
+        }
+    }
+}
